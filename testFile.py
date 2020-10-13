3663445b1c274c52577fd0203a2d57270b7d9442
Youssef Mohamed <youssefmohamed@Youssefs-Air-4-32.home>
2020-10-13 14:39:47 -0400

    Updated conv functions.

--- testFile.py
+++ testFile.py
@@ -18,29 +18,33 @@ import time
 # f=cnv.conv(a,b,2,1)
 # print(d,e,f)
 
-# z=np.array([[[ 1.0, 2.0, 3.0],
-#             [ 0.0, 0.0, 0.0],
-#             [-1.0,-2.0,-3.0]],[[ 1.0, 0.0,-1.0],
-#                         [ 2.0, 0.0,-2.0],
-#                         [ 3.0, 0.0,-3.0]]])
-# y=np.array([[1,2,3,4,5],
+# z=np.array([[[ 1, 2, 3],
+#             [ 4, 3, 4],
+#             [-1,-2,-3]],[[ 1, 0,-1],
+#                         [ 2, 0,-2],
+#                         [ 3, 0,-3]]])
+# y=np.array([[[1,2,3,4,5],
 #             [5,4,3,2,1],
 #             [1,5,2,4,3],
 #             [3,2,4,1,5],
-#             [1,1,1,1,1]])
+#             [1,1,1,1,1]],[[1,2,3,4,5],
+#                         [5,4,3,2,1],
+#                         [1,5,2,4,3],
+#                         [3,2,4,1,5],
+#                         [0,0,1,0,0]]])
 #
-# x=cnv.convManyH(z,y)
+# x=cnv.conv(z,y,nDim=2)
 # print(z)
 # print(y)
 # print(x)
-# w=cnv.conv(z,y,0)
-# v=cnv.conv(z,y,1)
-# u=cnv.conv(z,y,np.array([1,2]),0)
-# print(w)
-# print(v)
-# print(u)
-# t=cnv.conv(z,y,np.array([1,2]),1)
-# print(t)
+# # w=cnv.conv(z,y,0)
+# # v=cnv.conv(z,y,1)
+# # u=cnv.conv(z,y,np.array([1,2]),0)
+# # print(w)
+# # print(v)
+# # print(u)
+# # t=cnv.conv(z,y,np.array([1,2]),1)
+# # print(t)
 
 # z=np.array([[[ 1, 1, 1],
 #             [ 0, 0, 0],
@@ -75,59 +79,56 @@ import time
 #                                                             [5,4,3,2,1],
 #                                                             [1,5,2,4,3]]])
 #
-# x=cnv.convManyH(z,y)
+# x=cnv.conv(z,y)
 # print(z)
 # print(y)
 # print(x)
-# w=cnv.convManyH(z,y,0)
-# v=cnv.convManyH(z,y,1)
-# u=cnv.convManyH(z,y,np.array([1,2,2]),0)
+# w=cnv.conv(z,y,0)
+# v=cnv.conv(z,y,1)
+# u=cnv.conv(z,y,np.array([1,2,2]),0)
 # print(w)
 # print(v)
 # print(u)
-# t=cnv.convManyH(z,y,np.array([1,2,2]),1)
+# t=cnv.conv(z,y,np.array([1,2,2]),1)
 # print(t)
 
 im = Image.open("TestImages/TI_0001.jpg")
 print(im.format, im.size, im.mode)
-im.show()
+# im.show()
 
 imArr = np.moveaxis(np.array(im),2,0)
 print(imArr.shape)
 
-kernal = np.array([ [ 2, 2, 2, 2, 2],
+kernals=np.array([[ [ 2, 2, 2, 2, 2],
                     [ 1, 1, 1, 1, 1],
                     [ 0, 0, 0, 0, 0],
                     [-1,-1,-1,-1,-1],
-                    [-2,-2,-2,-2,-2]])
-tic = time.perf_counter()
-imArrFil = cnv.conv(kernal,imArr, 0)
-toc = time.perf_counter()
-print(f"Computed 3 channel image convolution in {toc - tic:0.4f} seconds")
+                    [-2,-2,-2,-2,-2]],[ [ 2, 1, 0,-1,-2],
+                                        [ 2, 1, 0,-1,-2],
+                                        [ 2, 1, 0,-1,-2],
+                                        [ 2, 1, 0,-1,-2],
+                                        [ 2, 1, 0,-1,-2]]])
 
-imFil = Image.fromarray(np.moveaxis(cnv.toInt(np.abs(imArrFil),imArr.dtype),0,2))
+
+# numR = 50
+# imArrFil = cnv.conv(kernals,imArr, 0)
+# tic = time.perf_counter()
+# for i in range(numR):
+#     imArrFil = cnv.conv(kernals,imArr, 0, nDim=2)
+# toc = time.perf_counter()
+# print(f"Conv: {(toc - tic)/(numR*2):0.4f} seconds")
+
+imArrFil = cnv.conv(kernals,imArr, 0, nDim=2)
+imFil = Image.fromarray(np.moveaxis(cnv.toInt(np.abs(imArrFil[0,...]),imArr.dtype),0,2))
+imFil.show()
+imFil.save("TestImages/HImage.jpg")
+imFil = Image.fromarray(np.moveaxis(cnv.toInt(np.abs(imArrFil[1,...]),imArr.dtype),0,2))
 imFil.show()
 imFil.save("TestImages/VImage.jpg")
 
-
-kernal = kernal.T
-tic = time.perf_counter()
-imArrFil = cnv.conv(kernal,imArr, 0)
-toc = time.perf_counter()
-print(f"Computed 3 channel image convolution in {toc - tic:0.4f} seconds")
-
-imFil = Image.fromarray(np.moveaxis(cnv.toInt(np.abs(imArrFil),imArr.dtype),0,2))
-imFil.show()
-imFil.save("TestImages/HImage.jpg")
-
 greyImArr =np.array(im.convert('L'))
-tic = time.perf_counter()
-greyImArrFil = cnv.conv(np.array([kernal,kernal.T]),greyImArr, 0)
-toc = time.perf_counter()
-print(f"Computed 2 filter image convolution in {toc - tic:0.4f} seconds")
-
+greyImArrFil = cnv.conv(kernals,greyImArr, 0)
 GIA=np.mean(greyImArrFil,0)
-
 imFil = Image.fromarray(cnv.toInt(np.abs(GIA),imArr.dtype),'L')
 imFil.show()
 imFil.save("TestImages/VHImage.jpg")
